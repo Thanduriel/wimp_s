ac3adab5447793331e1c008e2340ffd3771bf5a5
Thanduriel <rojendersie@alice.de>
2017-06-26 16:15:27 +0200

    # reworked component management + added class to generate simple Actors with one component

--- src/gameplay/core/singlecomponentactor.hpp
+++ src/gameplay/core/singlecomponentactor.hpp
@@ -6,10 +6,16 @@
 
 namespace Game {
 
+	/* SingleComponentActor *****************************
+	 * Template to generate Actor-types with just one component attached to them.
+	 * Handles management and creation of the component.
+	 */
 	template<class T>
 	class SingleComponentActor : public Actor, public T
 	{
 	public:
+		// Takes the position of the actor
+		// and forwards all other arguments to the component.
 		template<typename... Args>
 		SingleComponentActor(const ei::Vec3& _position, Args&&... _args)
 			: Actor(_position),
@@ -21,6 +27,7 @@ namespace Game {
 			_sceneGraph.RegisterComponent(component_cast<T>(*this));
 		}
 	protected:
+		// simpler name for this type to perform constructor inheritance.
 		typedef SingleComponentActor<T> SuperType;
 	};
 }
